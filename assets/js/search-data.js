0a7e5b4e84992747581de6effdfa056a70e83ada
fareselkhoury <157112164+fareselkhoury@users.noreply.github.com>
2025-09-22 03:09:49 +0000

    Deploying to gh-pages from @ fareselkhoury/fareselkhoury.github.io@97452d67c928e087cae26f9fe22aa50a16ba4c7a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,14 +391,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-learning-theory-for-kernel-bilevel-optimization-was-accepted-at-neurips-2025",
           title: 'Learning Theory for Kernel Bilevel Optimization was accepted at NeurIPS 2025!',
           description: "",
