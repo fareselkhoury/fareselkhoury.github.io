9992166f9b19c2af232e35d865eefd3404c85393
fareselkhoury <157112164+fareselkhoury@users.noreply.github.com>
2025-11-14 16:01:19 +0000

    Deploying to gh-pages from @ fareselkhoury/fareselkhoury.github.io@8b9b65a12c0c2d089289f93966c192ba9ea4666d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -401,12 +401,7 @@ ninja.data = [{
             },},{id: "news-learning-theory-for-kernel-bilevel-optimization-was-accepted-at-neurips-2025",
           title: 'Learning Theory for Kernel Bilevel Optimization was accepted at NeurIPS 2025!',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
@@ -441,6 +436,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-bilevel-optimization-for-data-cleaning",
+          title: 'Bilevel Optimization for Data Cleaning',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/data_cleaning/";
             },},{id: "projects-instrumental-variable-regression-using-bilevel-optimization",
           title: 'Instrumental Variable Regression using Bilevel Optimization',
           description: "NeurIPS 2025",
